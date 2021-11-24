22543eb04c403fb490e4c632eaaa8e4ca91b01ff
Marco Meyer (www) <marco.meyerconde@gmail.com>
2021-11-24 20:16:16 +0100

    Update services

---
+++ Twig/CrispTwigExtension.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Symfony\UX\Crisp\Twig;
+
+use Twig\Extension\AbstractExtension;
+
+class GtmTwigExtension extends AbstractExtension
+{
+    public function __construct() {
+
+        /* Add some Google Analytics summary twig functions, globals or filters? */
+    }
+}
