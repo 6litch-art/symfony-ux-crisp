e56026bb6698fad0500a4072a30543e03b50963b
Marco Meyer (www) <marco.meyerconde@gmail.com>
2023-09-07 15:57:39 +0200

    Fix session issue

--- Resources/public/crisp.js
+++ Resources/public/crisp.js
@@ -1,25 +1,24 @@
 window.addEventListener("load", function()
 {
+    function uniqid(prefix = "", random = false) {
+        const sec = Date.now() * 1000 + Math.random() * 1000;
+        const id = sec.toString(16).replace(/\./g, "").padEnd(14, "0");
+        return `${prefix}${id}${random ? `.${Math.trunc(Math.random() * 100000000)}`:""}`;
+    };
+
     var crispData = document.getElementById("crisp-script").dataset;
-    
-    function getCookie(name) {
-        var re = new RegExp(name + "=([^;]+)"); 
-        var value = re.exec(document.cookie); 
-        return (value != null) ? unescape(value[1]) : null; 
-    }
 
-    CRISP_RUNTIME_CONFIG = {session_merge : true, locale : crispData.locale};
-    CRISP_WEBSITE_ID = crispData.websiteId;
-    CRISP_TOKEN_ID = this.localStorage.getItem("crisp-client/token/"+CRISP_WEBSITE_ID) || 
-                     getCookie("crisp-client%2Fsession%2F"+CRISP_WEBSITE_ID);
+    window.CRISP_RUNTIME_CONFIG = {session_merge : true, locale : crispData.locale};
+    window.CRISP_WEBSITE_ID = crispData.websiteId;
+    window.CRISP_TOKEN_ID = crispData.userId || this.localStorage.getItem("crisp-client/token/"+window.CRISP_WEBSITE_ID);
+    if(window.CRISP_TOKEN_ID == null) window.CRISP_TOKEN_ID = uniqid("", true);
 
-    this.localStorage.setItem("crisp-client/token/"+CRISP_WEBSITE_ID, CRISP_TOKEN_ID);
+    this.localStorage.setItem("crisp-client/token/"+window.CRISP_WEBSITE_ID, window.CRISP_TOKEN_ID);
 
     if(typeof $crisp != "undefined")
         $crisp.push(["do", "session:reset", [false]]);
 
     window.$crisp = [];
-    
     (function () {
         d = document;
         s = d.createElement("script");
