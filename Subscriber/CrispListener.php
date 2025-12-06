1d273b548ff9af3a93c765f2333ec83a6b721a50
Marco Meyer-Conde <marco.meyerconde@gmail.com>
2025-12-07 00:12:33 +0900

    update listener

--- Subscriber/CrispListener.php
+++ Subscriber/CrispListener.php
@@ -64,7 +64,7 @@ class CrispListener
 
     public function isProfiler($event)
     {
-        $route = $event->getRequest()->get('_route');
+        $route = $event->getRequest()->attributes->get('_route');
         return str_starts_with($route ?? "", "_wdt") || str_starts_with($route ?? "", "_profiler");
     }
 
